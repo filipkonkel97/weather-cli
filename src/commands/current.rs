55d18c5e45c2e55388d09cc9c87916257e7c0e4e
Filip Konkel <filipkonkel97@gmail.com>
2026-07-04 16:27:14 +0200

    feat: save OpenWeather API key in login command

--- src/commands/current.rs
+++ src/commands/current.rs
@@ -1,6 +1,9 @@
+use crate::config::Config;
 use anyhow::Result;
 
 pub async fn run(city: String) -> Result<()> {
+    let cfg = Config::load()?;
+
     println!("Weather for {}", city);
 
     Ok(())
--- src/commands/login.rs
+++ src/commands/login.rs
@@ -1,6 +1,21 @@
+use crate::config::Config;
 use anyhow::Result;
+use std::io;
 
 pub fn run() -> Result<()> {
     println!("Please enter your OpenWeather API key:");
+
+    let mut api_key = String::new();
+
+    io::stdin().read_line(&mut api_key)?;
+
+    let cfg = Config {
+        api_key: api_key.trim().to_string(),
+    };
+
+    cfg.save()?;
+
+    println!("API key saved!");
+
     Ok(())
 }
